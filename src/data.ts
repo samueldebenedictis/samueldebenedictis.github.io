26c095f730080267a2858122d0d0e6b089617d3b
sdebenedictis <samuel.debenedictis@facile.it>
2023-06-08 16:57:47 +0200

    wip: data

---
+++ src/data.ts
@@ -0,0 +1,50 @@
+import { GalleryProps, BottomProps } from "./types";
+
+export const dependenciesData: BottomProps = {
+	items: [
+		{
+			name: "canvas-sketch",
+			link: "https://github.com/mattdesl/canvas-sketch",
+		},
+		{ name: "p5.js", link: "https://p5js.org/" },
+		{ name: "chroma.js", link: "https://www.vis4.net/chromajs/" },
+	],
+};
+
+export const itemsData: GalleryProps = {
+	items: [
+		{
+			img: require("./assets/2023.06.07-21.27.25.png"),
+			title: "Breakfast",
+		},
+		{
+			img: require("./assets/2023.06.07-21.27.25.png"),
+			title: "Burger",
+		},
+		{
+			img: require("./assets/2023.06.07-21.27.25.png"),
+			title: "Breakfast",
+		},
+		{
+			img: require("./assets/2023.06.07-21.27.25.png"),
+			title: "Burger",
+		},
+		{
+			img: require("./assets/2023.06.07-21.27.25.png"),
+			title: "Breakfast",
+		},
+		{
+			img: require("./assets/2023.06.07-21.27.25.png"),
+			title: "Burger",
+		},
+		{
+			img: require("./assets/2023.06.07-21.27.25.png"),
+			title: "Breakfast",
+		},
+		{
+			img: require("./assets/2023.06.07-21.27.25.png"),
+			title: "Burger",
+		},
+	],
+};
+export {};
